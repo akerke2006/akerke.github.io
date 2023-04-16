a3f1e1e716792cefd7a63a3b7914c1cb8c3d322c
akerke2006 <112396809+akerke2006@users.noreply.github.com>
2023-04-16 15:16:56 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function MyFunction() {
+
+
+	var name = prompt("age")
+	if (name >18) {
+		alert("youre old")
+	}else if(name <= 18){
+		alert("not that old")
+	}
+
+
+	alert("LA LA LA LA LAAAAAAAAAAA")
+	}
